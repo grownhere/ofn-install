297187843c2655799a5ecb516f4c2eb40fd4573a
Jordan "sloth" Mock <j.mock_@outlook.com>
2025-06-25 10:56:28 +0100

    Configure GrownHere production and testing environments with infrastructure updates

--- community/coopdevs.certbot_nginx/molecule/default/tests/test_default.py
+++ community/coopdevs.certbot_nginx/molecule/default/tests/test_default.py
@@ -10,11 +10,10 @@ def test_certbot_is_installed(host):
     certbot = host.package("certbot")
 
     assert certbot.is_installed
-    assert certbot.version.startswith("0.31")
+
 
 
 def test_certbot_nginx_is_installed(host):
     certbot_nginx = host.package("python-certbot-nginx")
 
     assert certbot_nginx.is_installed
-    assert certbot_nginx.version.startswith("0.31")
